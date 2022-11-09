efd7d4c4e2fa3dafd6981cc0b372d87ce792749e
unknown <pedro.beschorendacosta@wur.nl>
2022-11-09 11:21:42 +0100

    Update on several points of workshop structure

--- code/install_packages_load_libraries.R
+++ code/install_packages_load_libraries.R
@@ -6,17 +6,11 @@ if (!require("BiocManager", quietly = TRUE))
 if (!require("devtools", quietly = TRUE))
   install.packages("devtools")
 
-# if (!require("RCurl", quietly = TRUE))
-#  install.packages("RCurl")
-
 if (!require("phyloseq", quietly = TRUE))
   BiocManager::install("phyloseq")
 
 if (!require("metacoder", quietly = TRUE))
   install.packages("metacoder")
-
-if (!require("vegan", quietly = TRUE))
-  install.packages("vegan")
 
 if (!require("dplyr", quietly = TRUE))
   install.packages("dplyr")
@@ -27,28 +21,25 @@ if (!require("tidyverse", quietly = TRUE))
 if (!require("tibble", quietly = TRUE))
   install.packages("tibble")
 
-library(devtools)
-if (!require("vmikk/metagMisc", quietly = TRUE))
+library(devtools) # you need this package to isntall the next package
+if (!require("metagMisc", quietly = TRUE))
   devtools::install_github("vmikk/metagMisc",quiet = TRUE)
 
 if (!require("purrr", quietly = TRUE))
   install.packages("purrr")
 
-if (!require("ggpubr", quietly = TRUE))
-  install.packages("ggpubr")
 
 
 
 #load libraries
 library(metacoder) # base package we will work with
 library(phyloseq) # essential data container
-library(vegan) # perofrms some basic operations
 library(metagMisc) # turns ps objects into lists of ps objects
 library(purrr) # has map() to extract values from lists
 library(dplyr)
 library(tibble)
 library(tidyverse)
-library(ggpubr)
+
 
 
 
